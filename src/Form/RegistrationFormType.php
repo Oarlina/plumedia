2a0747918aded2fd301139be9a21784270412130
Rachel Marquant <marquantrachel@gmail.com>
2025-04-22 15:18:17 +0200

    Ajout CSS de l'inscription et la connexion

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -10,6 +10,8 @@ use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
@@ -18,17 +20,17 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('pseudo')
-            ->add('email')
+            ->add('pseudo', TextType::class)
+            ->add('email', EmailType::class)
             ->add('avatar', FileType::class)
             ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
+                            'mapped' => false,
+                            'constraints' => [
+                                new IsTrue([
+                                    'message' => 'You should agree to our terms.',
+                                ]),
+                            ],
+                        ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
