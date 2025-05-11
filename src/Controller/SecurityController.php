b6edc7cc84dab712b730c4b7b82cadb9372babe6
Rachel Marquant <marquantrachel@gmail.com>
2025-05-12 00:04:40 +0200

    Possibilité de changer d'info hors mot de passe

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -33,5 +38,55 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route(path: '/changeMailAvatar', name: 'changeMailAvatar', methods: ['POST', 'FILE'])]
+    public function changeMailAvatar (Request $request, UserRepository $userRepository,Filesystem $filesystem, EntityManagerInterface $entityManager): Response{
+        $error = "non"; // pour la gestion de message si il y a une erreur
+        // je recupere l'email, le pseudo et l'avatar du formulaire
+        // je n'est pas a verifier les types car ils sont forcer dans l'entité User
+        $email = $request->request->get('email');
+        $pseudo = $request->request->get('pseudo');
+        $file = $request->files->get('avatar');
+        // je recupere les infos de l'utilisateur connecter 
+        $user = $this->getUser();
+
+        // on verifie que le mail est different et  dans la BDD
+        if ($email != $user->getEmail() && ($userRepository->findBy(['email'=> $email]) != null)){
+            $error = "oui";
+        }else{
+            $user = $user->setEmail($email);
+        }
+        // on verifie que le pseudo est different et dans la BDD
+        if ($pseudo !=  $user->getPseudo() && ($userRepository->findBy(['pseudo'=> $pseudo]) != null)) {
+            $error = "oui";
+        }else{
+            $user = $user->setPseudo($pseudo);
+        }
+        // on verifie que le fichier est envoyé 
+        if ($file){
+            // si l'extension est jpg, jpeg, svg, png ou webp alors je l'enregistre sinon erreur
+            if ($file->guessExtension() == "jpg" || $file->guessExtension() == "jpeg" || $file->guessExtension() == "svg" || $file->guessExtension() == "png" || $file->guessExtension() == "webp"){
+                // je le renomme et recupere l'extension
+                $newFile = 'user-'.uniqid().'.'.$file->guessExtension();
+                $file->move('uploads/user/', $newFile);
+
+                // si l'utilisateur a un avatar alors je supprime le fichier du dossier uploads
+                if ($user->getAvatar()){
+                    $filesystem->remove('uploads/user/'.$user->getAvatar());
+                }
+                $user->setAvatar($newFile);
+            } else {
+                $this->addFlash('error', 'Le format du fichier n\'est pas le bon.');
+            }
+        }
+        
+        // je met a jour la base de données, je faais la gestion d'erreur puis retourne sur la page de profil
+        $entityManager->persist($user);
+        $entityManager->flush();
+        if ($error == "oui"){
+            $this->addFlash('error', 'Problème lors de la modification des informations.');
+        }
+        return $this->redirectToRoute('app_profil');
     }
 }
