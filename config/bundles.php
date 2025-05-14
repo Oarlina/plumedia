c7f86ce588501cce2bf9b393cd837b1c43efe9fa
Rachel Marquant <marquantrachel@gmail.com>
2025-05-14 08:50:14 +0200

    Gestion mot de passe oublié

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
     Karser\Recaptcha3Bundle\KarserRecaptcha3Bundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
